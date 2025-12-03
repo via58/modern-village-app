94fb1fe94d56e086848bf004e990824922ca5355
via58 <asp.vijaybaskar@gmail.com>
2025-12-03 15:41:05 +0530

    Fix the build errors

---
+++ app/donate/page.js
@@ -0,0 +1,24 @@
+
+// Sample React component
+'use client';
+import React, { useState } from 'react';
+
+const SampleDonateComponent = () => {
+  const [count, setCount] = useState(0);
+
+  const handleIncrement = () => {
+    setCount(count + 1);
+  };
+
+  return (
+    <div>
+      <h1>Sample Component</h1>
+      <p>Count: {count}</p>
+      <button onClick={handleIncrement}>
+        Increment
+      </button>
+    </div>
+  );
+};
+
+export default SampleDonateComponent;
